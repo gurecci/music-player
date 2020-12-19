00a22ab5326b75c327ffc6c9964d8ca568f39a2f
gurecci <fatihgureci2@gmail.com>
2020-12-19 14:34:40 +0300

    Creating Our Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+//Adding Components
+import Player from "./components/Player";
+import Song from "./components/Song";
 
 function App() {
   return (
     <div className="App">
-
+      <Song />
+      <Player />
     </div>
   );
 }
