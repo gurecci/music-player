08d8e98786c56d60391e2b01a817b7f6daa4219a
gurecci <fatihgureci2@gmail.com>
2021-01-02 21:39:07 +0300

    Adding Our State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import { React, useState } from "react";
 //Import Styles
-import './styles/app.scss'
+import "./styles/app.scss";
 //Adding Components
 import Player from "./components/Player";
 import Song from "./components/Song";
@@ -8,9 +8,13 @@ import Song from "./components/Song";
 import chillHop from "./data";
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(chillHop());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
