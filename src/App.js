9c4bb85e4a4fc31ad02255f7bc71e1c958f666b1
gurecci <fatihgureci2@gmail.com>
2021-01-02 21:48:53 +0300

    Use Ref

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player />
+      <Player currentSong={currentSong} />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
@@ -6,7 +6,14 @@ import {
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({currentSong}) => {
+  //Ref
+  const audioRef = useRef(null)
+  //Event Handlers
+  const playSongHandler = () => {
+    console.log(audioRef.current)
+  }
+
   return (
     <div className="player">
       <div className="time-control">
@@ -16,13 +23,14 @@ const Player = () => {
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
         <FontAwesomeIcon
           className="skip-forward"
           size="2x"
           icon={faAngleRight}
         />
       </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
     </div>
   );
 };
